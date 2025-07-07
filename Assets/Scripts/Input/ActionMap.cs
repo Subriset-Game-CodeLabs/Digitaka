a30b48bbf0e2f75d3266587fbbd46feb95aa5958
aprnna <121468657+aprnna@users.noreply.github.com>
2025-07-07 14:02:54 +0700

    setup project

---
+++ Assets/Scripts/Input/ActionMap.cs
@@ -0,0 +1,56 @@
+
+using UnityEngine;
+
+namespace Input
+{
+    public abstract class ActionMap: IState
+    {
+        protected InputActions InputActions;
+        public abstract bool HasPollable { get; }
+        public ActionMap(InputActions action)
+        {
+            InputActions = action;
+        }
+        public abstract void OnEnter();
+
+        public abstract void OnExit();
+
+        public virtual void OnUpdate()
+        {
+        }
+    }
+
+
+    
+    public class PlayerActionMap : ActionMap
+    {
+        private InputValue<Vector2> _movement;
+        private InputButton _attack;
+        public InputButton Attack => _attack;
+        public InputValue<Vector2> Movement => _movement;
+
+        public override bool HasPollable => true;
+
+        public PlayerActionMap(InputActions action) : base(action)
+        {
+            _movement = new InputValue<Vector2>(action.Player.Move);
+            _attack = new InputButton(action.Player.Attack);
+        }
+
+
+        public override void OnEnter()
+        {
+            InputActions.Player.Enable();
+        }
+
+        public override void OnExit()
+        {
+            InputActions.Player.Disable();
+        }
+        public override void OnUpdate()
+        {
+            _movement.ForcePoll();
+        }
+    }
+    
+}
---
+++ Assets/Scripts/State/FiniteStateMachine.cs
@@ -0,0 +1,28 @@
+public class FiniteStateMachine<T> where T : IState
+{
+    private T _currentState;
+    private T _previousState;
+
+    public FiniteStateMachine(T entry)
+    {
+        _currentState = entry;
+        _currentState.OnEnter();
+    }
+
+    public void ChangeState(T newState)
+    {
+        if (ReferenceEquals(newState, _currentState))
+        {
+            return;
+        }
+        _previousState = _currentState;
+        _previousState.OnExit();
+        _currentState = newState;
+        _currentState.OnEnter();
+    }
+
+    public void OnUpdate()
+    {
+        _currentState.OnUpdate();
+    }
+}
